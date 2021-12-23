ea334aa5d0428f5b149c39ad1bdbd97b1c21c707
Vlad Tansky <vl.tansky@gmail.com>
2021-12-23 14:31:33 +0200

    feat: init example

---
+++ components/Input.jsx
@@ -0,0 +1,5 @@
+export default function Input() {
+  return (
+    <input className="transition duration-300 border-b-2 border-gray-300 w-80 hover:border-blue-400 focus:outline-none focus:shadow-outline" />
+  );
+}
